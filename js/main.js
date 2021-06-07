559fc10f363c6eead265fc419e538cf01618590b
max.b <max.b@dev.izibook.ru>
2021-06-08 01:00:08 +0300

    добавил условие сравнения min и max значений

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function randomNumber(min, max) {
-  if(max < 0) {
+  if(max < 0 || min === max) {
     return;
   }
-  if(min < 0) {
+  if(min < 0 ) {
     min = 0;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function randomNumberFloat(min, max, quantity) {
-  if(max < 0) {
+  if(max < 0 || min === max) {
     return;
   }
   if(min < 0) {
