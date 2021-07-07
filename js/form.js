944190834dfa188a16e2f7bac6d428e379fc0f4a
max.b <max.b@dev.izibook.ru>
2021-07-07 22:49:43 +0300

    Внес правки в модуль form

--- js/form.js
+++ js/form.js
@@ -4,38 +4,27 @@ const fieldsetHeader = form.querySelector('.ad-form-header');
 const mapFiltersBlock = document.querySelector('.map__filters');
 const mapFeatures = mapFiltersBlock.querySelector('.map__features');
 const mapFilterElement = mapFiltersBlock.querySelectorAll('.map__filter');
-function setAttributeDisabledElement(elem, name, value, type) {
-  if(type === 'notAll') {
-    elem.setAttribute(name, value);
+function toggledAttrebuteDisabled(element) {
+  if(element.hasAttribute('disabled')) {
+    element.removeAttribute('disabled');
   }else {
-    elem.forEach((el)=>{
-      el.setAttribute(name, value);
-    });
-  }
-}
-function setAttributeActivateElement(elem, attrName, type) {
-  if(type === 'notAll') {
-    elem.removeAttribute(attrName);
-  }else {
-    elem.forEach((el)=>{
-      el.removeAttribute(attrName);
-    });
+    element.setAttribute('disabled', 'disabled');
   }
 }
 function disabledForm() {
   form.classList.add('ad-form--disabled');
   mapFiltersBlock.classList.add('ad-form--disabled');
-  setAttributeDisabledElement(fildsetFormElement,'disabled', 'disabled', 'all');
-  setAttributeDisabledElement(mapFilterElement,'disabled', 'disabled');
-  setAttributeDisabledElement(fieldsetHeader,'disabled', 'disabled', 'notAll');
-  setAttributeDisabledElement(mapFeatures,'disabled','disabled', 'notAll');
+  fildsetFormElement.forEach((el) => toggledAttrebuteDisabled(el));
+  mapFilterElement.forEach((el) => toggledAttrebuteDisabled(el));
+  toggledAttrebuteDisabled(fieldsetHeader);
+  toggledAttrebuteDisabled(mapFeatures);
 }
 function activateForm() {
   form.classList.remove('ad-form--disabled');
   mapFiltersBlock.classList.remove('ad-form--disabled');
-  setAttributeActivateElement(fildsetFormElement,'disabled', 'all');
-  setAttributeActivateElement(mapFilterElement,'disabled', 'all');
-  setAttributeActivateElement(fieldsetHeader,'disabled', 'notAll');
-  setAttributeActivateElement(mapFeatures,'disabled', 'notAll');
+  fildsetFormElement.forEach((el) => toggledAttrebuteDisabled(el));
+  mapFilterElement.forEach((el) => toggledAttrebuteDisabled(el));
+  toggledAttrebuteDisabled(fieldsetHeader);
+  toggledAttrebuteDisabled(mapFeatures);
 }
 export {disabledForm, activateForm};
