f5a49c34f8d3100ee46957e272c9efc3fc6d5d94
max.b <max.b@dev.izibook.ru>
2021-07-16 23:36:29 +0300

    Внес правки в карту

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ import {disabledForm, activateForm,disabledAllOptionsRoom, iniOptionsGroupRoomSe
 const mapCanvas = document.querySelector('#map-canvas');
 const mapAddress = document.querySelector('#address');
 const map = L.map(mapCanvas);
+const MAIN_PIN_MARKER_LAT = 35.658581;
+const MAIN_PIN_MARKER_LNG = 139.745438;
+const MAIN_PIN_ICON_SIZE = [52, 52];
+const MAIN_PIN_ICON_ANCHOR = [26, 52];
+const PIN_ICON_SIZE = [40, 40];
+const PIN_ICON_ANCHOR = [20, 40];
+const MAP_VIEW_LAT = 35.658581;
+const MAP_VIEW_LNG = 139.745438;
+const MAP_VIEW_SCALE = 10;
 const data = genirationOrders(10);
 disabledForm();
 disabledAllOptionsRoom();
@@ -12,13 +21,13 @@ priceСhangePlaceholder();
 
 const mainPinIcon = L.icon({
   iconUrl: './img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
+  iconSize: MAIN_PIN_ICON_SIZE,
+  iconAnchor: MAIN_PIN_ICON_ANCHOR,
 });
 const mainPinMarker =
   L.marker({
-    lat: 35.658581,
-    lng: 139.745438,
+    lat: MAIN_PIN_MARKER_LAT,
+    lng: MAIN_PIN_MARKER_LNG,
   },
   {
     draggable: true,
@@ -26,17 +35,17 @@ const mainPinMarker =
   });
 const pinIcon = L.icon({
   iconUrl: './img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
+  iconSize: PIN_ICON_SIZE,
+  iconAnchor: PIN_ICON_ANCHOR,
 });
 
 map
   // eslint-disable-next-line no-unused-vars
   .on('load', () =>  activateForm())
   .setView({
-    lat: 35.658581,
-    lng: 139.745438,
-  }, 12);
+    lat: MAP_VIEW_LAT,
+    lng: MAP_VIEW_LNG,
+  }, MAP_VIEW_SCALE);
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   {
